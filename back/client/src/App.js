1f802eff70652880b62d08b88e12c1a4a04922d4
zoutigo <zoutigo@gmail.com>
2020-10-16 11:12:26 +0200

    app: structure. front renamed client , client moved inside back

---
+++ back/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is my very first app on Heroku</h1>
+    </div>
+  );
+}
+
+export default App;
