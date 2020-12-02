f50a3a950afebdbd30ca28bf02786e98cc7f0f2a
zoutigo <zoutigo@gmail.com>
2020-12-02 13:45:00 +0100

    initialize sub navigation for large screens

--- client/src/components/header/NavItem.js
+++ client/src/components/header/NavItem.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React , {useState} from 'react'
 import {makeStyles} from '@material-ui/styles'
-import {Typography} from '@material-ui/core'
-import {useLocation} from 'react-router-dom'
+import {Button, Typography} from '@material-ui/core'
+import {NavLink, useLocation} from 'react-router-dom'
+
 
 const useStyles = makeStyles((theme)=>({
     root :{
@@ -40,28 +41,78 @@ const useStyles = makeStyles((theme)=>({
       
         color: theme.palette.success.main,
         
+    },
+    submenu : {
+        background : 'yellow'
+    },
+    hide : {
+        display: 'none'
+    },
+    show : {
+        display: 'block'
+    },
+    dropdown: {
+        overflow: 'hidden',
+        '&:hover':{
+            background:'red',
+            '& div':{
+                display: 'block'
+            }
+        }
+    },
+    dropdownContent :{
+    
+        position:'absolute',
+        zIndex: 1
     }
+    
     
 }))
 
-function NavItem({name , icon , path}) {
+function NavItem({name , icon , path, subitems}) {
     const classes = useStyles()
     const location = useLocation()
+
+    const [hover, setHover] = useState(false)
 
     const activeRoot = (path === location.pathname) ? classes.rootActive : classes.rootNotActive
     const activeIcon = (path === location.pathname) ? classes.iconActive : classes.iconNotActive
     const activeBottom = (path === location.pathname) ? classes.bottomActive : classes.bottomNotActive
     const activeText = (path === location.pathname) ? classes.textActive : classes.textNotActive
-  
+
+    const showSubMenu = hover ? classes.show : classes.hide
+
+
     return (
-        <div className={`${classes.root} ${activeRoot}`}>
-          <div className={`${classes.icon} ${activeIcon}`}> {icon} </div>
-          <div className={classes.textbox}> 
-             <Typography variant='h6' className={activeText}> {name} </Typography>
-          </div>
-          <div className={activeBottom}> </div>
-    
+        <div  className={classes.dropdown}>
+                <div
+                className={`${classes.root} ${activeRoot}`}
+                onMouseOver = {()=> setHover(true)}
+                onMouseOut = { ()=> setHover(false)}
+               
+                >
+                    <div className={`${classes.icon} ${activeIcon}`}> {icon} </div>
+                    <div className={classes.textbox}> 
+                        <Typography variant='h6' className={activeText}> {name} </Typography>
+                    </div>
+                    <div className={activeBottom}> </div>
+                </div>
+             
+                <div className={`${classes.dropdownContent} ${showSubMenu}`} >
+                   {
+                       subitems && subitems.map((item, index)=>{
+                           return (
+                               <NavLink key={index} to={item.link} >
+                                   <div> {item.designation} </div>
+                               </NavLink>
+                           )
+                       })
+                   }
+                </div>
+                
+                
         </div>
+        
     )
 }
 
--- client/src/components/header/Navigation.js
+++ client/src/components/header/Navigation.js
@@ -2,8 +2,10 @@ import React from 'react'
 import {useSelector} from 'react-redux'
 import {NavLink} from 'react-router-dom'
 
+
+
 import {makeStyles} from '@material-ui/styles'
-import {Box} from '@material-ui/core'
+import {Box, Button} from '@material-ui/core'
 
 import NavItem from './NavItem'
 
@@ -21,23 +23,30 @@ function Navigation() {
     const classes = useStyles()
     const navElements = useSelector(state => state.settings.navElements)
 
+  
+
     return (
-        <Box className={classes.root}>
+        <Box className={classes.root} onClick={()=> console.log('Hello all')}>
           
            {
                navElements.map((element, index)=> {
                    if(element.alias !=='home') {
                       
                        return (
-                           <NavLink key={index} to={element.link} style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                               <NavItem name={element.name} icon={element.icon} path={element.link} />
-                           </NavLink>
+                           
+                              
+                               <NavLink key={index} to={element.link} style={{ color: 'inherit', textDecoration: 'inherit'}}>
+                                     <NavItem name={element.name} icon={element.icon} path={element.link} subitems={element.sub} />
+                                </NavLink>
+                            
                        )
                        
                    }
                    return null
                })
            }
+          
+
         </Box>
     )
 
