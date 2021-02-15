1ad6f9a3eb5cf05ca7695943601f3a11b1acd476
zoutigo <zoutigo@gmail.com>
2021-02-15 08:33:36 +0100

    check response from team endpoint

--- client/src/components/content/ecole/team/Team.js
+++ client/src/components/content/ecole/team/Team.js
@@ -5,7 +5,13 @@ import { apiFecthTeam } from '../../../../utils/api'
 import Group from './group/Group'
 
 function Team() {
+  const Groups = [
+    { department: 'Les enseignants', members: [] },
+    { department: 'La direction', members: [] },
+  ]
+  const [groups, setGroups] = React.useState(Groups)
   const { isLoading, isError, data, error } = useQuery('team', apiFecthTeam)
+
   if (isLoading) {
     return <span>Loading...</span>
   }
@@ -14,15 +20,16 @@ function Team() {
     return <span>Error: {error.message}</span>
   }
 
+  console.log(data)
   return (
     <Grid container>
-      {data.map((group, index) => {
+      {/* {data.map((group, index) => {
         return (
           <Grid item container key={index}>
             <Group {...group} />
           </Grid>
         )
-      })}
+      })} */}
     </Grid>
   )
 }
