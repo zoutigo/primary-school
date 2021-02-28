eed2e061e63df0b9e146d4e5c80ee0de3776fef4
zoutigo <zoutigo@gmail.com>
2021-02-28 20:26:12 +0100

    set fs to create images directory

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const multer = require("multer");
+const fs = require("fs");
 const {
   createImage,
   listImages,
@@ -11,7 +12,13 @@ const {
 
 var storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "./public/images");
+    const dir = "./public/images";
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+
+    callback(null, dir);
   },
   filename: function (req, file, callback) {
     callback(null, Date.now() + "_" + file.originalname);
