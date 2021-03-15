34577597a9fbe0e174ac073654a18262d75a84c7
zoutigo <zoutigo@gmail.com>
2021-03-15 13:57:38 +0100

    create endoit variable for netlify no access to env variables

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+ENDPOINT = 'https://ecole-saint-augustin.herokuapp.com'
+
 export const apiCheckEmail = async (value) => {
   const data = { email: value }
   let url = 'https://ecole-saint-augustin.herokuapp.com/users/checkemail'
@@ -50,54 +52,56 @@ export const apiFecthUserDatas = async (id) => {
 }
 
 export const apiFecthTeam = async () => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/users/team`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/users/team`
   let result = await axios.get(URL)
   return result
 }
 export const apiFecthPage = async (obj) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/pages/${obj.queryKey[1].alias}`
+  let URL = `${pprocess.env.REACT_APP_ENDPOINT || ENDPOINT}/pages/${
+    obj.queryKey[1].alias
+  }`
   let { data } = await axios.get(URL)
 
   return data
 }
 export const apiCreatePage = async (obj) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/pages`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/pages`
   let result = await axios.post(URL, obj.body, obj.options)
   return result
 }
 
 export const apiFecthAllPages = async () => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/pages`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/pages`
   let { data } = await axios.get(URL)
   return data
 }
 
 export const apiUpdatePage = async (obj) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/pages/${obj.id}`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/pages/${obj.id}`
   let { data } = await axios.put(URL, obj.body, obj.options)
   return data
 }
 export const apiUpdateClassroom = async ({ id, body, options }) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/classrooms/${id}`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/classrooms/${id}`
   let { data } = await axios.put(URL, body, options)
   return data
 }
 
 export const apiFecthClassroom = async (param) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/classrooms/${param}`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/classrooms/${param}`
   let { data } = await axios.get(URL)
 
   return data
 }
 export const apiFetchPaper = async (param) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/papers/${param}`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/papers/${param}`
   let { data } = await axios.get(URL)
 
   return data
 }
 
 export const apiPostPaper = async ({ id, body, options }) => {
-  let URL = `${process.env.REACT_APP_ENDPOINT}/papers/${id}`
+  let URL = `${process.env.REACT_APP_ENDPOINT || ENDPOINT}/papers/${id}`
   let { data } = await axios.post(URL, body, options)
   return data
 }
