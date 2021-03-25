f37da346046caec5311d04a7567a91a4de9a4fe1
zoutigo <zoutigo@gmail.com>
2021-03-25 13:27:24 +0100

    update cors policy

--- server/app.js
+++ server/app.js
@@ -57,16 +57,16 @@ app.use(
     exposedHeaders: ["x-access-token"],
   })
 );
-app.all("", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  //Auth Each API Request created by user.
-  next();
-});
+// app.all("", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   //Auth Each API Request created by user.
+//   next();
+// });
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb", extended: true }));
