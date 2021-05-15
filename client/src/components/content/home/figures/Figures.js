0c624114e8e6f7af193080061de4a807568210df
zoutigo <zoutigo@gmail.com>
2021-05-15 06:11:21 +0200

    fix bubbles responsive behaviour

--- client/src/components/content/home/figures/Figures.js
+++ client/src/components/content/home/figures/Figures.js
@@ -17,42 +17,18 @@ const StyledFiguresContainer = styled(Grid)(() => ({
 }))
 
 const matrix = [
-  [
-    [100, 50],
-    [100, 50],
-    [100, 200],
-    [10, '10vw'],
-    [10, '30vw'],
-  ],
-  [
-    [500, 700],
-    [500, 700],
-    [500, 700],
-    [180, '40vw'],
-    [180, '30vw'],
-  ],
-  [
-    [200, 1100],
-    [200, 1100],
-    [200, 1100],
-    [360, '10vw'],
-    [350, '30vw'],
-  ],
-  [
-    [400, 1500],
-    [400, 1500],
-    [400, 1500],
-    [520, '70vw'],
-    [520, '30vw'],
-  ],
+  ['6vh', '8vw'],
+  ['50vh', '35vw'],
+  ['20vh', '55vw'],
+  ['35vh', '70vw'],
 ]
 
 function Figures() {
   const items = [
-    ['Eleves', 218, 0, '#85C48E', matrix[0]],
-    ['Familles', 100, 1, '#C389BC', matrix[1]],
-    ['Enseignants', 25, 2, '#EE701D', matrix[2]],
-    ['Jeux', 49, 3, '#F9CE46', matrix[3]],
+    ['Eleves', 218, '#85C48E', matrix[0]],
+    ['Familles', 100, '#C389BC', matrix[1]],
+    ['Enseignants', 25, '#EE701D', matrix[2]],
+    ['Jeux', 49, '#F9CE46', matrix[3]],
   ]
 
   return (
--- client/src/components/content/home/figures/FiguresCard.js
+++ client/src/components/content/home/figures/FiguresCard.js
@@ -2,59 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Avatar, Box, Typography } from '@material-ui/core'
 import { styled, withTheme } from '@material-ui/styles'
-import { useWindowSize } from '../../../../utils/hooks'
-
-const initialPositions = [
-  [100, 200],
-  [500, 700],
-  [200, 1100],
-  [400, 1500],
-]
-
-const matrix = [
-  [
-    [100, 50],
-    [100, 50],
-    [100, 200],
-    [100, 200],
-    [100, 200],
-  ],
-  [
-    [500, 700],
-    [500, 700],
-    [500, 700],
-    [500, 700],
-    [500, 700],
-  ],
-  [
-    [200, 1100],
-    [200, 1100],
-    [200, 1100],
-    [200, 1100],
-    [200, 1100],
-  ],
-  [
-    [400, 1500],
-    [400, 1500],
-    [400, 1500],
-    [400, 1500],
-    [400, 1500],
-  ],
-]
-
-const StyledFigureCard = withTheme(
-  styled(({ positions, theme, ...rest }) => <Box {...rest} />)({
-    position: 'absolute',
-    [(theme) => theme.breakpoints.up('lg')]: {
-      top: ({ positions }) => positions[0][0],
-      left: ({ positions }) => positions[0][1],
-    },
-    [(theme) => theme.breakpoints.between('md', 'lg')]: {
-      top: ({ positions }) => positions[1][0],
-      left: ({ positions }) => positions[1][1],
-    },
-  })
-)
 
 const StyledAvatar = withTheme(
   styled(({ bgcolor, ...rest }) => <Avatar {...rest} />)({
@@ -77,22 +24,12 @@ const StyledDesignationTypo = styled(Typography)(() => ({
 }))
 
 function FiguresCard({ figureitem }) {
-  const [designation, count, index, bgcolor, positions] = figureitem
+  const [designation, count, bgcolor, positions] = figureitem
 
-  const StyledFigureCards = styled(Box)(({ theme }) => ({
+  const StyledFigureCards = styled(Box)(() => ({
     position: 'absolute',
-    [theme.breakpoints.up('lg')]: {
-      top: positions[0][0],
-      left: positions[0][1],
-    },
-    [theme.breakpoints.between('xs', 'sm')]: {
-      top: positions[3][0],
-      left: positions[3][1],
-    },
-    [theme.breakpoints.down('xs')]: {
-      top: positions[4][0],
-      left: positions[4][1],
-    },
+    top: positions[0],
+    left: positions[1],
   }))
   return (
     <StyledFigureCards positions={positions}>
